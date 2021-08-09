0cd3b4c8349042e965eb79d8d2d455f53bc97384
Shingo Kato <katomail0510@gmail.com>
2021-08-09 10:06:25 +0000

    Fix for rust 1.53.0 / emcc 1.39.20

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
 
 extern crate core;
 
 extern "C" {
-    pub fn emscripten_GetProcAddress(name: *const std::os::raw::c_char)
-        -> *const std::os::raw::c_void;
+    pub fn emscripten_GetProcAddress(
+        name: *const std::os::raw::c_char,
+    ) -> *const std::os::raw::c_void;
 }
 
 include!(concat!(env!("OUT_DIR"), "/emscripten.rs"));
